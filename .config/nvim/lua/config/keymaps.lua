ee614505a11916f14556daa55a64f0dcde698164
gokul <gokulviswanathan25@gmail.com>
2025-09-23 01:06:44 -0400

    fix nvim lsp issues

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,8 +8,19 @@ keymap.set('i', 'jk', '<Esc>', { noremap = true })
 keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename variable" })
 keymap.set("n", "<leader>/", ":noh<cr>", { desc = "Remove highlight" })
 
-vim.keymap.set('n', 'gd', '<cmd>vsplit | lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n', 'gD', '<cmd>split | lua vim.lsp.buf.definition()<CR>')
+-- vim.keymap.set('n', 'gd', '<cmd>vsplit | lua vim.lsp.buf.definition()<CR>')
+-- vim.keymap.set('n', 'gD', '<cmd>split | lua vim.lsp.buf.definition()<CR>')
+local function lsp_def_in(cmd)
+  return function()
+    vim.cmd(cmd)
+    vim.lsp.buf.definition()
+  end
+end
+
+vim.keymap.set("n", "gd", lsp_def_in("vsplit"), { desc = "Definition vsplit" })
+vim.keymap.set("n", "gD", lsp_def_in("split"), { desc = "Definition split" })
+vim.keymap.set("n", "gt", lsp_def_in("tabnew"), { desc = "Definition tab" })
+
 vim.keymap.set('n', 'gr', vim.lsp.buf.references)
 vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { noremap = true, silent = true })
 -- vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = { "lua_ls", "ts_ls", "basedpyright", },
-                automatic_enable = false,
+                automatic_installation = true, -- Changed from automatic_enable
             })
         end
     },
@@ -27,6 +27,9 @@ return {
                                 typeCheckingMode = "standard",
                             },
                         },
+                        python = {
+                            pythonPath = "python", -- Add default python path
+                        },
                     },
                     on_new_config = function(config, root_dir)
                         local venv_path = root_dir .. "/venv/bin/python"
@@ -42,11 +45,77 @@ return {
         },
         config = function(_, opts)
             local lspconfig = require('lspconfig')
+
+            -- Get capabilities from your completion plugin
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            if pcall(require, 'blink.cmp') then
+                capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+            end
+
+            -- Setup each server
             for server, config in pairs(opts.servers) do
-                config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+                -- Merge capabilities
+                config.capabilities = vim.tbl_deep_extend('force', capabilities, config.capabilities or {})
+
+                -- Use the traditional lspconfig setup
                 -- lspconfig[server].setup(config)
                 vim.lsp.config(server, config)
             end
         end
     },
 }
+
+
+
+-- return {
+--     {
+--         "mason-org/mason.nvim",
+--         config = function()
+--             require("mason").setup()
+--         end
+--     },
+--     {
+--         "mason-org/mason-lspconfig.nvim",
+--         config = function()
+--             require("mason-lspconfig").setup({
+--                 ensure_installed = { "lua_ls", "ts_ls", "basedpyright", },
+--                 automatic_enable = false,
+--             })
+--         end
+--     },
+--     {
+--         "neovim/nvim-lspconfig",
+--         opts = {
+--             servers = {
+--                 lua_ls = {},
+--                 ts_ls = {},
+--                 basedpyright = {
+--                     settings = {
+--                         basedpyright = {
+--                             analysis = {
+--                                 typeCheckingMode = "standard",
+--                             },
+--                         },
+--                     },
+--                     on_new_config = function(config, root_dir)
+--                         local venv_path = root_dir .. "/venv/bin/python"
+--                         if vim.fn.executable(venv_path) == 1 then
+--                             config.settings.python.pythonPath = venv_path
+--                         end
+--                     end,
+--                 },
+--                 jdtls = {},
+--                 gopls = {},
+--                 ruff = {},
+--             }
+--         },
+--         config = function(_, opts)
+--             local lspconfig = require('lspconfig')
+--             for server, config in pairs(opts.servers) do
+--                 config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+--                 -- lspconfig[server].setup(config)
+--                 vim.lsp.config(server, config)
+--             end
+--         end
+--     },
+-- }
